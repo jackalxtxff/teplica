d1865900fcf5d5e0720aaa4a9864a1590527eae9
jackalxtxff <mar.adamson.29@gmail.com>
2020-06-05 17:50:11 +0400

    update #11 || catalog pages

--- product.php
+++ product.php
@@ -12,8 +12,44 @@
 <main class="transition-fade" id="swup">
 	<div class="container">
     <div class="breadcrumbs">
-        <p><a href="/catalog">Каталог</a> / <a><?= $product['product_name'] ?></a></p>
+        <p><a href="/catalog" style="color: green;">Каталог</a> / <a style="color: #59A857;"><?= $product['product_name'] ?></a></p>
     </div>
+    <section class="product row">
+      <div class="product-image col s12 m6">
+        <img src="<?= $product['image_path'] ?>" alt="">
+      </div>
+      <div class="product-feature col s12 m6">
+        <div class="product_name">
+          <?= $product['product_name'] ?>
+        </div>
+        <div class="vendor_code">
+          Артикул: <?= $product['vendor_code'] ?>
+        </div>
+        <div class="product_price">
+          <?= $product['product_price'] ?> руб
+        </div>
+        <div class="row">
+          <button class="button-product col s12 m8">ЗАКАЗАТЬ</button>
+        </div>
+
+      </div>
+      <div class="row">
+        <div class="product_about col s12 m6">
+          <p class="title-about">О товаре</p>
+          <div class="description">
+            <div class="product_description">
+              <p class="title">Описание:</p><?= $product['description'] ?>
+            </div>
+            <div class="material_type">
+              <p class="title">Тип материала:</p> <?= $product['material_type'] ?>
+            </div>
+            <div class="product_size">
+              <p class="title">Размеры:</p> <?= $product['product_size'] ?>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </main>
 
